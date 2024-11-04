d20ec9b54df62270b5b784349cd4623bab905ccd
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-11-04 17:20:54 +0200

    update email send

---
+++ js/form-button-send.js
@@ -0,0 +1,21 @@
+document.querySelector('.question__form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    fetch(this.action, {
+        method: 'POST',
+        body: new FormData(this),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+            alert('Форма відправлена!');
+            console.log('ok')
+            this.reset(); // Очищуємо форму
+        } else {
+            alert('Сталася помилка.');
+        }
+    }).catch(error => {
+        alert('Сталася помилка: ' + error.message);
+    });
+});
