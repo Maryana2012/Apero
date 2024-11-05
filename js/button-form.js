81aeca285d785dcf63b61206882e212d08250357
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-11-05 16:48:14 +0200

    update all submit buttons

--- js/button-form.js
+++ js/button-form.js
@@ -1,24 +1,7 @@
-
-// (() => {
-//     const refs = {
-//       openModalBtn: document.querySelectorAll("[data-modal-open]"),
-//       closeModalBtn: document.querySelector("[data-modal-close]"),
-//       modal: document.querySelector("[data-modal]"),
-//     };
-//     //  console.log(refs)
-//     refs.openModalBtn.addEventListener("click", toggleModal);
-//     refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    // function toggleModal() {
-    //   refs.modal.classList.toggle("is-hidden");
-    // }
-//   })();
-
 const openModalBtn = document.querySelectorAll('.modal-open');
 const closeModalBtn = document.querySelector('.modal-close');
 const modal = document.querySelector('.backdrop');
 
-closeModalBtn.addEventListener("click", toggleModal);
 openModalBtn.forEach(button=> button.addEventListener("click",
   (event=>{
     const clickedButton = event.currentTarget;
@@ -26,9 +9,13 @@ openModalBtn.forEach(button=> button.addEventListener("click",
     if(clickedButton){
       toggleModal();
     }
-  
+    
   })
 ))
+
+if(closeModalBtn){
+  closeModalBtn.addEventListener("click", toggleModal);
+} 
 
 function toggleModal() {
   modal.classList.toggle("is-hidden");
--- js/form-button-send.js
+++ js/form-button-send.js
@@ -1,21 +1,27 @@
-document.querySelector('.question__form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    fetch(this.action, {
-        method: 'POST',
-        body: new FormData(this),
-        headers: {
-            'Accept': 'application/json'
-        }
-    }).then(response => {
-        if (response.ok) {
-            alert('Форма відправлена!');
-            console.log('ok')
-            this.reset(); // Очищуємо форму
-        } else {
-            alert('Сталася помилка.');
-        }
-    }).catch(error => {
-        alert('Сталася помилка: ' + error.message);
+const closeForm = document.querySelector('.backdrop');
+document.querySelectorAll('.question__form').forEach(function(form) {
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        fetch(this.action, {
+            method: 'POST',
+            body: new FormData(this),
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                alert('Форма відправлена!');
+                console.log('ok');
+                this.reset(); // Очищуємо форму
+                if(closeForm){
+                    closeForm.classList.add('is-hidden')
+                }
+            } else {
+                alert('Сталася помилка.');
+            }
+        }).catch(error => {
+            alert('Сталася помилка: ' + error.message);
+        });
     });
 });
