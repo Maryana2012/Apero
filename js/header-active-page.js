ca5a5eadd95220cbe2fcb5332a7a0af2a6b03294
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-11-05 17:36:47 +0200

    adding google maps

--- js/header-active-page.js
+++ js/header-active-page.js
@@ -1,8 +1,21 @@
 const currentPage = window.location.pathname;
+const pointCurrentPage = "." + currentPage;
 const navLinks = document.querySelectorAll('.nav__link');
+// console.log(navLinks);
+// console.log(currentPage)
 
 navLinks.forEach(link => {
-    if (link.getAttribute('href') === currentPage){
+    // console.log(link)
+    if (link.getAttribute('href') === pointCurrentPage){
         link.classList.add('active__link')
     }
 } )
+
+// navLinks.forEach(link => {
+//     // console.log(link)
+//     // if (link.getAttribute('href') === currentPage){
+//        console.log(link.getAttribute('href'))
+//        console.log(currentPage)
+//         // link.classList.add('active__link')
+//     // }
+// } )
