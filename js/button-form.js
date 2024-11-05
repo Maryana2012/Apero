995094e9cc403f65d8649a5e9913711656b269dc
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-11-05 11:58:04 +0200

    close modal

--- js/button-form.js
+++ js/button-form.js
@@ -1,15 +1,35 @@
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-    //  console.log(refs)
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelectorAll("[data-modal-open]"),
+//       closeModalBtn: document.querySelector("[data-modal-close]"),
+//       modal: document.querySelector("[data-modal]"),
+//     };
+//     //  console.log(refs)
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+    // function toggleModal() {
+    //   refs.modal.classList.toggle("is-hidden");
+    // }
+//   })();
+
+const openModalBtn = document.querySelectorAll('.modal-open');
+const closeModalBtn = document.querySelector('.modal-close');
+const modal = document.querySelector('.backdrop');
+
+closeModalBtn.addEventListener("click", toggleModal);
+openModalBtn.forEach(button=> button.addEventListener("click",
+  (event=>{
+    const clickedButton = event.currentTarget;
+    
+    if(clickedButton){
+      toggleModal();
     }
-  })();
+  
+  })
+))
+
+function toggleModal() {
+  modal.classList.toggle("is-hidden");
+}
