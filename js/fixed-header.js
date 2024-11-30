ebbedf9836d6147afef83cc4edc13e62da849d57
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-11-30 19:30:26 +0200

    about section hero is addaptived

--- js/fixed-header.js
+++ js/fixed-header.js
@@ -3,6 +3,8 @@ const headerLinks = document.querySelectorAll('.nav__link');
 const headerButton = document.querySelector('.header__button');
 const headerLogo = document.querySelector('.logo');
 const headerLogoFixed = document.querySelector('.logo__fixed');
+const headerIconMenu = document.querySelector('.icon__menu__mobile');
+
 
 const fixedHeader = () =>{
     if(window.scrollY > 0){
@@ -12,14 +14,16 @@ const fixedHeader = () =>{
         headerLogo.classList.remove('.logo');
         headerLogo.classList.add('logo__fixed');
         headerLogoFixed.classList.remove('logo__fixed');
-        headerLogoFixed.classList.add('logo')
+        headerLogoFixed.classList.add('logo');
+        headerIconMenu.classList.add('icon__menu__mobile-second');
     } else { header.classList.remove('header__second') 
              headerLinks.forEach(link => {link.classList.remove('nav__link-second')});
              headerButton.classList.remove('header__button-second');
              headerLogo.classList.add('.logo');
              headerLogo.classList.remove('logo__fixed');
              headerLogoFixed.classList.add('logo__fixed');
-             headerLogoFixed.classList.remove('logo')
+             headerLogoFixed.classList.remove('logo');
+             headerIconMenu.classList.remove('icon__menu__mobile-second');
 
 }
 }
