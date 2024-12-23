c8a85606a8bef57296100afc8ce2e3b35d5f9fe1
Maryana2012 <114877890+Maryana2012@users.noreply.github.com>
2024-12-23 11:18:42 +0200

    add  aria-label

--- js/active-page-mob-menu.js
+++ js/active-page-mob-menu.js
@@ -1,9 +1,12 @@
 const mobileMenu = document.querySelectorAll('.menu__link');
 const activePage = document.querySelector('.active__link')
 
-const activeMobileMenu = mobileMenu.forEach(el=>{
-    if(el.text.toLowerCase() === activePage.text.toLowerCase() ){
-    el.remove();
-    } 
-})
+if (activePage){
+    const activeMobileMenu = mobileMenu.forEach(el=>{
+        if(el.text.toLowerCase() === activePage.text.toLowerCase() ){
+        el.remove();
+        } 
+    })
+
+}
 
--- js/filter-main-page.js
+++ js/filter-main-page.js
@@ -8,36 +8,39 @@ const buds = document.querySelectorAll('.buds__list');
 const syharyky = document.querySelectorAll('.suharyky__list');
 const bambuk = document.querySelectorAll('.bambuc__list');
 
-if (filter.includes('popcorn')){
-    popcorn.forEach(popcornItem => popcornItem.classList.remove('not-active'));
+if (filter){
+
+  if (filter.includes('popcorn')){
+      popcorn.forEach(popcornItem => popcornItem.classList.remove('not-active'));
+      snacks.forEach(snack => snack.classList.add('not-active'));
+      corn.forEach(cornItem => cornItem.classList.add('not-active'));
+      buds.forEach(bud => bud.classList.add('not-active'));
+      syharyky.forEach(syharyky=>syharyky.classList.add('not-active'));
+      bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
+  }
+  
+  if(filter.includes('sweet')){
+      corn.forEach(cornItem => cornItem.classList.remove('not-active'));
     snacks.forEach(snack => snack.classList.add('not-active'));
-    corn.forEach(cornItem => cornItem.classList.add('not-active'));
+    popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
     buds.forEach(bud => bud.classList.add('not-active'));
     syharyky.forEach(syharyky=>syharyky.classList.add('not-active'));
     bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
-}
-
-if(filter.includes('sweet')){
-    corn.forEach(cornItem => cornItem.classList.remove('not-active'));
-  snacks.forEach(snack => snack.classList.add('not-active'));
-  popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
-  buds.forEach(bud => bud.classList.add('not-active'));
-  syharyky.forEach(syharyky=>syharyky.classList.add('not-active'));
-  bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
-}
-if(filter.includes('pods')){
-    buds.forEach(bud => bud.classList.remove('not-active'));
-  snacks.forEach(snack => snack.classList.add('not-active'));
-  corn.forEach(cornItem => cornItem.classList.add('not-active'));
-  popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
-  syharyky.forEach(syharyky=>syharyky.classList.add('not-active'));
-  bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
-}
-if(filter.includes('syharyky')){
-    buds.forEach(bud => bud.classList.add('not-active'));
-  snacks.forEach(snack => snack.classList.add('not-active'));
-  corn.forEach(cornItem => cornItem.classList.add('not-active'));
-  popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
-  syharyky.forEach(syharyky=>syharyky.classList.remove('not-active'));
-  bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
+  }
+  if(filter.includes('pods')){
+      buds.forEach(bud => bud.classList.remove('not-active'));
+    snacks.forEach(snack => snack.classList.add('not-active'));
+    corn.forEach(cornItem => cornItem.classList.add('not-active'));
+    popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
+    syharyky.forEach(syharyky=>syharyky.classList.add('not-active'));
+    bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
+  }
+  if(filter.includes('syharyky')){
+      buds.forEach(bud => bud.classList.add('not-active'));
+    snacks.forEach(snack => snack.classList.add('not-active'));
+    corn.forEach(cornItem => cornItem.classList.add('not-active'));
+    popcorn.forEach(popcornItem => popcornItem.classList.add('not-active'));
+    syharyky.forEach(syharyky=>syharyky.classList.remove('not-active'));
+    bambuk.forEach(bambuk=>bambuk.classList.add('not-active'));
+  }
 }
